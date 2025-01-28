c560412831eb03bd14bda424cc89496e0bc88b6e
Clayton Sweeten <cjsweeten101@gmail.com>
2025-01-28 13:51:02 -0700

    slight re-factor

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,21 +1,5 @@
 module Enumerable
   # Your code goes here
-end
-
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
-class Array
-  # Define my_each here
-  def my_each
-    for i in 0..self.length - 1 do
-      yield(self[i])
-    end
-
-    self
-  end
-
   def my_select
     result = []
     self.my_each do |elem|
@@ -45,5 +29,24 @@ class Array
 
   def my_none?    
     !self.my_any? {|elem| yield(elem)}
+  end
+
+  def my_count
+    
+  end
+end
+
+# You will first have to define my_each
+# on the Array class. Methods defined in
+# your enumerable module will have access
+# to this method
+class Array
+  # Define my_each here
+  def my_each
+    for i in 0..self.length - 1 do
+      yield(self[i])
+    end
+
+    self
   end
 end
